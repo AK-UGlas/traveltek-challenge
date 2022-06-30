2bf55876aa2a07e202dddece5ecdabc987522df9
AK-UGlas <allenkelly278@gmail.com>
2022-06-30 23:38:24 +0100

    adds banner header to front page

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import '../styles/Header.css';
+
+export const Header = () => {
+
+    return (
+        <div className="header">
+            <a className="logo" href="https://www.traveltek.com/" target="_blank"></a>
+            <h2>Traveltek coding challenge</h2>
+        </div>
+    );
+};
+
+export default Header;
